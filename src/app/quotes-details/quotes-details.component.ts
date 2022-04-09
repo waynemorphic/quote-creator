10ff3ec739ac39ceccd9a450f933a98cf9378e37
waynemorphic <wayneklyde@gmail.com>
2022-04-09 15:19:01 +0300

    Displayed the quotes description and changed quotes color to white

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quotes-details',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesDetailsComponent implements OnInit {
 
+  @Input () qts:Quotes;
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+// Here we are doing input proprty binding from 
+// new-quotes parent component to quotes-details child component
+
+//qts is the property that is undergoing input property binding, receiving
+// data from new-quotes component
