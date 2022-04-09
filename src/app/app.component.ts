34edfe9d0f8b787088a5fed2a2155652150110d6
waynemorphic <wayneklyde@gmail.com>
2022-04-09 13:11:57 +0300

    Permently transferred quotes array to new-quotes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import { Quotes } from './quotes';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  quotes:Quotes[] = [
-    {quote: 'It always seems impossible until it is done', name: 'Nelson Mandela' },
-    {quote: 'Be inspired before you expire', name:'Pepe Minambo'}
-  ];
-}
 
+export class AppComponent {
+//   qts:Quotes[] = [
+//     {quote: 'It always seems impossible until it is done', name: 'Nelson Mandela' },
+//     {quote: 'Be inspired before you expire', name:'Pepe Minambo'}
+//   ];
+}
+// console.log(Quotes.quote)
 //imported the Quotes blue print class
 //quotes is an object attributed to the Quotes class
