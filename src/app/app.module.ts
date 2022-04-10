21b2b816a9d6c72cf4ac6b3f6a17807488c8a680
waynemorphic <wayneklyde@gmail.com>
2022-04-10 21:52:23 +0300

    Several changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewQuotesComponent } from './new-quotes/new-quotes.component';
 import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { DateCountPipe } from './date-count.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewQuotesComponent,
-    QuotesDetailsComponent
+    QuotesDetailsComponent,
+    QuotesFormComponent,
+    DateCountPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule //added form module after importing it since it is not available in the angular app by default
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/new-quotes/new-quotes.component.ts
+++ src/app/new-quotes/new-quotes.component.ts
@@ -11,13 +11,32 @@ export class NewQuotesComponent implements OnInit {
   
 
   qts:Quotes[] = [
-    new Quotes('It always seems impossible until it is done', 'Nelson Mandela', 'Posted by Wayne'),
-    new Quotes('Be inspired before you expire', 'Pepe Minambo', 'Posted by Wayne'),
+    new Quotes('It always seems impossible until it is done', 'Nelson Mandela', 'Posted by Wayne', new Date(2022, 4, 7)),
+    new Quotes('Be inspired before you expire', 'Pepe Minambo', 'Posted by Wayne', new Date (2022, 4, 1)),
   ];
   toggleQuoteDescription(index:any){
       this.qts[index].showDescription = !this.qts[index].showDescription;
-    }
+    };
+  
+  quoteDone(isDone:any, index:any){ //we want to delete a quote if user prompts so
+    if (isDone){
+      this.qts.splice(index,1)
+    };
+  };
 
+  addNewQuote(nQ: any) {
+    let qtsArraySize = this.qts.length;
+    nQ.id = qtsArraySize+1;
+    nQ.completeDate = new Date(nQ.completeDate)
+    this.qts.push(nQ)
+
+    
+    console.log(nQ.description)
+    // console.log(nQ.name)
+    
+    
+  }
+    
   constructor() { }
 
   ngOnInit() {
@@ -27,3 +46,10 @@ export class NewQuotesComponent implements OnInit {
 // qts is a property which is an array of type Quotes blueprint class
 
 //new is a keyword for calling class Quotes and defining each quote
+
+//toggleQuoteDescription is a function that binds an event so that the 
+//parent and child components are able to communicate. in this case,
+//they bind the event showDescription.
+
+//quoteDone is a function that catches emitted events from the child
+// component and parent Component. we pass isDone EventEmitter and index as the arguments
